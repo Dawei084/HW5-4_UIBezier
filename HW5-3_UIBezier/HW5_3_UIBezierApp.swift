e5b0243b30cd71f2c09bc44fc606baafdb805ee0
Daweihao84 <dwsamurai84@gmail.com>
2023-01-16 22:28:01 +0800

    Initial Commit

---
+++ HW5-3_UIBezier/HW5_3_UIBezierApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW5_3_UIBezierApp.swift
+//  HW5-3_UIBezier
+//
+//  Created by Dawei Hao on 2023/1/16.
+//
+
+import SwiftUI
+
+@main
+struct HW5_3_UIBezierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
